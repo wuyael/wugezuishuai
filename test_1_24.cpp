972fd2f309a7b85cecdd5ae1da823c2e408cb4b9
wuyael <156645539+wuyael@users.noreply.github.com>
2024-01-24 19:49:52 +0800

    Add files via upload

--- test_1_24.cpp
+++ test_1_24.cpp
@@ -2,23 +2,39 @@
 #include<stdio.h>
 #include<string.h>
 
-void point_run(int nian)
+void halfsearch(int arr[], int k)
 {
-	if ((0 == nian % 4) && (0 != nian % 100))
+	int left = 0;
+	int right = sizeof(arr) / sizeof(arr[0]-1);
+	while (left <= right)
 	{
-		printf("%d是闰年", nian);
+		int mid = (left + right) / 2;
+			if (arr[mid] < k)
+			{
+				left = mid + 1;
+			}
+			else if (arr[mid] > k)
+			{
+				right = mid - 1;
+			}
+			else
+			{
+				printf("找到了，下标是%d\n", mid);
+				break;
+			}
 	}
-	else
+	if (right < left)
 	{
-		printf("%d不是闰年", nian);
+		printf("输入错误，未找到\n");
 	}
 }
 
 int main(void)
 {
-	int a = 0;
-	scanf("%d",&a);
-	point_run(a);
+	int arr[] = { 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
+	int k = 0;
+	scanf("%d", &k);
+	halfsearch(arr, k);
 
 	return 0;
 }
