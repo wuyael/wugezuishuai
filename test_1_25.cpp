cfc606cd1903e6380d8f89da0948355a02e5f337
wuyael <156645539+wuyael@users.noreply.github.com>
2024-01-25 17:33:04 +0800

    Add files via upload

--- test_1_25.cpp
+++ test_1_25.cpp
@@ -1,12 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<string.h>
-
-int jiecheng(int x)
+//斐波那契数列的递归和递归写法
+int rabbit_digui(int n)
 {
-	if (1 !=x )
+	if (n>2)
 	{
-		return x * jiecheng(x - 1);
+		return rabbit_digui(n - 1) + rabbit_digui(n - 2);
+	}
+	else
+	{
+		return 1;
+	}
+}
+int rabbit_cycle(int n)
+{
+	int a = 1;
+	int b = 1;
+	int num = 0;
+	if (n > 2)
+	{
+		for (; n - 2 > 0; n--)
+		{
+			num = a + b;
+			a = b;
+			b = num;
+		}
+		return num;
 	}
 	else
 	{
@@ -15,10 +35,10 @@ int jiecheng(int x)
 }
 int main(void)
 {
-	int x = 0;
-	for (x = 1; x <= 20; x++)
-	{
-		printf("%d\n", jiecheng(x));
-	}
+	int n = 0;
+	scanf("%d", &n);
+	printf("%d\n", rabbit_digui(n));
+	printf("%d", rabbit_cycle(n));
+
 	return 0;
 }
